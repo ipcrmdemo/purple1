e8c4c59c7c124d36be543d252a016aa68b86a272
Matt Cadorette <mcadorette@gmail.com>
2018-09-25 15:58:03 -0400

    Initial commit from Atomist

---
+++ src/main/java/purple1/Purple1Application.java
@@ -0,0 +1,12 @@
+package purple1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Purple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Purple1Application.class, args);
+	}
+}
